397098229f04bd8b4411a060a978918faf1bfa50
Wusi <chenwusi2012@gmail.com>
2022-11-01 22:01:44 -0700

    update

--- parser.py
+++ parser.py
@@ -2,22 +2,37 @@ import webvtt
 import pandas as pd
 
 if __name__ == '__main__':
+    update_start = True
     start = ''
     end = ''
-    total_text = ''
+    accu_text = ''
     df = pd.DataFrame(columns=['lecture', 'start_time', 'end_time', 'text'])
     filename = 'Lesson 1.2 Text Access.vtt'
     captions = webvtt.read('./subtitles/' + filename)
+    start = captions[0].start
     for idx, caption in enumerate(captions):
-        start_time = caption.start
-        end_time = caption.end
+        start_time = caption.start[0:-4]
+        end_time = caption.end[0:-4]
         text = caption.text
         text = text.replace('\n', ' ')
-        if text != '[SOUND]' or text != '[MUSIC]':
+        print("start time: " + start_time)
+        print("end time: " + end_time)
+        print("text: " + text)
+        if text != '[SOUND]' and text != '[MUSIC]':
+            accu_text = accu_text + text
+            end = end_time
+            if update_start:
+                start = start_time
             if text[-1] != '.':
-                total_text = total_text + text
+                update_start = False
+                print("continue")
+                print("total text: " + accu_text)
             else:
-                df = df.append({'lecture': filename, 'start_time': start_time, 'end_time': end_time, 'text': total_text}, ignore_index=True)
-                total_text = ''
+                print("find dot")
+                print("total text: " + accu_text)
+                df = df.append({'lecture': filename, 'start_time': start, 'end_time': end, 'text': accu_text}, ignore_index=True)
+                accu_text = ''
+                update_start = True
+        else:
+            update_start = True
     df.to_csv('./dataset/data.csv')
-    print(df.head(3))
---
+++ time_converter.py
@@ -0,0 +1,8 @@
+def convert_time(time_stamp):
+    values = time_stamp.split(':')
+    second = int(values[0]) * 3600 + int(values[1]) * 60 + int(values[2])
+    return values
+
+
+if __name__ == '__main__':
+    print(convert_time("00:01:32"))
